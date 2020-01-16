8ce1eb1abbe5da7e10e64b83cdc9c927903a865d
GGPeccatum <59838076+GGPeccatum@users.noreply.github.com>
2020-01-16 19:37:12 +0600

    Обьекты
    Дабавил обькты машины
    цвет
    скорость
    аудиосистема

--- script.js
+++ script.js
@@ -23,17 +23,37 @@
 
 
 //Console
-
+/*
 let a = 2;
 let b = 4;
 let c = 10;
-/*
+
 console.log(a);
 console.log(b);
 b = c;
 console.log(a+b);
-*/
+
  console.log("a=" ,a, ",b=" ,b, "c=", c)
  b = c;
  console.log("b=c")
  console.log("a+b=", a+b);
+
+ console.log(a);
+ console.log(typeof a);
+ */
+
+
+
+ //Обьекты
+let car = {
+    color: 'red',
+    maxSpeed: 220,
+    audio: {
+        brand: 'Sony',
+        speakers: 12
+    }
+};
+car.color = 'green';
+
+console.log(car);
+console.log("car color=",car.color);
