6d5f8e699533f03897f03091b52427b1d306107b
GGPeccatum <59838076+GGPeccatum@users.noreply.github.com>
2020-01-16 20:28:38 +0600

    Работа с переменными
    Задачи

--- script.js
+++ script.js
@@ -5,7 +5,8 @@
 // alert('И подключили его');
 
 
-//переменные
+//                                             переменные
+
 
 
 // let message;
@@ -22,38 +23,86 @@
 // alert(sms);
 
 
-//Console
+
+
+//                                              Console
+
+
+
+// let a = 2;
+// let b = 4;
+// let c = 10;
+
+// console.log(a);
+// console.log(b);
+// b = c;
+// console.log(a+b);
+
+//  console.log("a=" ,a, ",b=" ,b, "c=", c)
+//  b = c;
+//  console.log("b=c")
+//  console.log("a+b=", a+b);
+
+//  console.log(a);
+//  console.log(typeof a);
+
+
+
+
+ 
+
+//                                     Обьекты
+
+
+
+ 
+// let car = {
+//     color: 'red',
+//     maxSpeed: 220,
+//     audio: {
+//         brand: 'Sony',
+//         speakers: 12
+//     }
+// };
+// car.color = 'green';
+
+// console.log(car);
+// console.log("car color=",car.color);
+
+
+
+
+
+//                               работа с переменными 
+//ЗАДАЧИ 2
+
+
+
 /*
-let a = 2;
-let b = 4;
-let c = 10;
-
-console.log(a);
-console.log(b);
-b = c;
-console.log(a+b);
-
- console.log("a=" ,a, ",b=" ,b, "c=", c)
- b = c;
- console.log("b=c")
- console.log("a+b=", a+b);
-
- console.log(a);
- console.log(typeof a);
- */
+let name ='Джон';
+let admin = name;
+alert("Имя админа");
+alert(admin);
+*/
 
 
 
- //Обьекты
-let car = {
-    color: 'red',
-    maxSpeed: 220,
-    audio: {
-        brand: 'Sony',
-        speakers: 12
-    }
-};
-car.color = 'green';
 
-console.log(car);
-console.log("car color=",car.color);
+//                                 Задача 3
+//Создайте переменную для названия нашей планеты
+//Создайте переменную для хранения имени текущего посетителя сайта
+
+
+
+
+// let ourPlanetName = 'Земля';
+// let currentUserName = "Джон";
+
+// alert(ourPlanetName);
+// alert(currentUserName);
+
+
+
+
+
+
