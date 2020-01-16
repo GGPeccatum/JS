1c5cd0d2266f11e9d17d2075e2c8fd1b26d0f89d
GGPeccatum <59838076+GGPeccatum@users.noreply.github.com>
2020-01-16 18:19:51 +0600

    Добавили переменные
    обьявление переменных.

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-alert ('Привет, Мир!');
-alert('Создали файл JS');
-alert('И подключили его');
+"use strict";
+
+// alert ('Привет, Мир!');
+// alert('Создали файл JS');
+// alert('И подключили его');
+
+
+//переменные
+
+let message;
+message = 'Hello!';
+
+alert(message); // показывает содержимое переменной
+
+let user = 'Vlad'
+,   age = '22'
+,   sms = message;
+
+alert(user);
+alert(age);
+alert(sms);
