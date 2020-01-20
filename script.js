5f08594edff315a91b11ff54b097869b21c68b12
GGPeccatum <59838076+GGPeccatum@users.noreply.github.com>
2020-01-21 01:01:58 +0600

    циклы

--- script.js
+++ script.js
@@ -484,3 +484,65 @@ alert(admin);
 //  alert(`I do not know you ${enterLogin}`)
 // }
 
+
+
+
+//                 Циклы  
+
+
+
+// for (let i = 1; i < 3; i++) { // выведет 0, затем 1, затем 2
+//      alert(i);
+//    }
+
+//    for (;;) {
+//      // будет выполняться вечно
+//    }
+
+
+
+// for (let i = 2; i<=10 ; i++){
+//      if (i % 2 == 0){
+//      alert (i);
+//      }
+     
+// }
+
+
+// let i = 0;
+// while (i < 3) { // выводит 0, затем 1, затем 2
+//   alert( i );
+//   i++;
+// }
+
+
+// for (let i = 0; i < 3; i++) {
+//      alert( `number ${i}!` );
+//    }
+
+
+// let i = 0;
+// while (i < 3){
+//      alert(`number ${i}`);
+//      i++;
+// }
+
+
+// let num;
+
+// do {
+//   num = prompt("Введите число, большее 100?", 0);
+// } while (num <= 100 && num);
+
+
+let n = 10;
+
+nextPrime:
+for (let i = 2; i <= n; i++) { // Для всех i...
+
+  for (let j = 2; j < i; j++) { // проверить, делится ли число..
+    if (i % j == 0) continue nextPrime; // не подходит, берём следующее
+  }
+
+  alert( i ); // простое число
+}
